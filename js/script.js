444680434959b76cc8644f8161438dccb1252529
VitaFrancesco <vitafrancesco00@gmail.com>
2024-11-06 15:24:33 +0100

    ADD: js manipulation of DOM with method get for receives the array of object

---
+++ js/script.js
@@ -0,0 +1,76 @@
+/**
+ * 
+ * @param {String} tagnName 
+ * @param {Array} classList 
+ * @param {Array} content 
+ * @param {Function} callback 
+ * @returns 
+ */
+function myCreateElement(
+    tagnName,
+    classList = [],
+    content = [],
+    callback = false
+  ) {
+    // Creo l'elemento
+    const el = document.createElement(tagnName);
+  
+    // Aggiungo le classi
+    if (classList.length > 0) {
+      el.classList.add(...classList);
+    }
+  
+    // Esegui la callback passando l'elemento
+    if (callback) {
+      callback(el);
+    }
+  
+    // Contenuto
+    if (Array.isArray(content)) {
+      for (let i = 0; i < content.length; i++) {
+        el.appendChild(content[i]);
+      }
+    } else if (content instanceof HTMLElement) {
+      el.appendChild(content);
+    } else if (typeof content === "string") {
+      el.innerHTML = content;
+    } else {
+      console.error("Non posso aggiungere l'elemento");
+    }
+    return el;
+};
+
+axios.get('https://jsonplaceholder.typicode.com/photos', { // oppure 'url...?_limit=6'
+    params: {
+        _limit: 6,
+    },
+})
+    .then((res) => {
+        const postsEl = res.data;
+        const fragment = document.createDocumentFragment();
+        createPosts(postsEl, fragment);
+        postContainer.appendChild(fragment);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+const postContainer = document.getElementById("posts-container");
+console.log(postContainer);
+
+function createPosts (array, frag) {
+    array.forEach(({ url, title }) => {
+        const postEl = myCreateElement('div', ["col-resp"],
+            [
+                myCreateElement('div', ['card'],
+                [
+                    myCreateElement('div', ['img-264px'],
+                    [
+                        myCreateElement('img', [], [], (el) => (el.src = url))
+                    ]),
+                    myCreateElement('p', ['text-post'],`${title}`)
+                ])
+            ]);
+        frag.appendChild(postEl);
+    });
+};
